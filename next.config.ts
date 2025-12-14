b99734417f0a3c59475cc37626e45fd01a741992
albiistafa <izzatalbi@student.ub.ac.id>
2025-12-15 01:55:49 +0700

    fix: CVE-2025-66478 vulnerability

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
